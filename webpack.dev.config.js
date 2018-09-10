ffb2978bd54563b32b02a58ba8c50fa5e345602e
Douglas Dong <ad546971975@gmail.com>
2018-09-11 01:08:36 +0800

    repo init

---
+++ webpack.dev.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: __dirname,
+        filename: "release/bundle.js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin ({
+            template: "./index.html",
+        })
+    ],
+    module: {
+        rules: [{
+            test:/\.js?$/,
+            exclude: /(node_modules)/,
+            loader: "babel-loader"
+        }]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "./release"),//根目录
+        open: true,//自动打开浏览器
+        port: 9000
+    }
+};
