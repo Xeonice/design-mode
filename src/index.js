b1fd0fb6f9b12df27d1bb7760990e8eec4e7feb0
Douglas Dong <ad546971975@gmail.com>
2018-09-11 18:25:39 +0800

    extend practice

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 class Person {
-    constructor(name) {
+    constructor(name, age) {
         this.name = name;
+        this.age = age;
     }
-    getName() {
-        return this.name;
+    eat () {
+        alert(`${this.name} eat something`);
+    }
+    speak() {
+        alert(`My name is ${this.name}, age ${this.age}`);
     }
 }
 
-let p = new Person("Douglas");
-alert(p.getName());
+//继承
+class Student extends Person {
+    constructor (name, age, number) {
+        super(name, age);//交给父类构造函数
+        this.number = number;//子类单独处理
+    }
+    study() {
+        alert(`${this.name} study ${this.number}`);
+    }
+}
+
+let xiaoming = new Student("xiaoming", 10, "A1");
+xiaoming.study();
+xiaoming.eat();
+
+let xiaohong = new Student("xiaohong", 11, "A2");
+xiaohong.study();
+xiaohong.eat();
